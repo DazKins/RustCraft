3bf663ffeecc8b5f1fa759ef66b214409dfe5f98
David Atkins <gamecake1@gmail.com>
2020-04-02 22:48:29 +0100

    Added tick timing control

--- graphics/src/engine.rs
+++ graphics/src/engine.rs
@@ -1,11 +1,11 @@
 use crate::window::Window;
 use crate::game_state::GameState;
+use std::time::{ Instant };
 
 pub struct EngineConfig {
     pub window_width: u32,
     pub window_height: u32
 }
- 
 
 pub struct Engine {
     window: Window
@@ -30,11 +30,26 @@ impl Engine {
     }
     
     fn run_loop(&mut self, game_state: &mut dyn GameState) {
+        const TICKS_PER_SECOND: i32 = 60;
+        const MILLIS_PER_TICK: f64 = 1000.0 / TICKS_PER_SECOND as f64;
+
+        let mut last_frame_time = Instant::now();
+        let mut delta = 0.0;
+
         while !self.window.was_close_requested {
             self.window.clear();
             self.window.process_events();
 
-            game_state.tick();
+            let elapsed_time = Instant::now().checked_duration_since(last_frame_time);
+            last_frame_time = Instant::now();
+            
+            delta = delta + (elapsed_time.unwrap().as_millis() as f64) / MILLIS_PER_TICK;
+
+            while delta >= 1.0 {
+                delta -= 1.0;
+                game_state.tick();
+            }
+
             game_state.render();
 
             self.window.close();
