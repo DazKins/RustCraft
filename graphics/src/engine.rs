59609ac206d97ace6bf7260884cdd6f4ca594b20
David Atkins <gamecake1@gmail.com>
2020-04-04 22:00:30 +0100

    Added FPS and UPS tracker, Improved tick fidelity

--- graphics/src/engine.rs
+++ graphics/src/engine.rs
@@ -31,26 +31,47 @@ impl Engine {
     
     fn run_loop(&mut self, game_state: &mut dyn GameState) {
         const TICKS_PER_SECOND: i32 = 60;
-        const MILLIS_PER_TICK: f64 = 1000.0 / TICKS_PER_SECOND as f64;
+        const NANOS_PER_TICK: f64 = 1000000000.0 / TICKS_PER_SECOND as f64;
+
+        let mut frame_tracker = 0;
+        let mut tick_tracker = 0;
 
         let mut last_frame_time = Instant::now();
         let mut delta = 0.0;
+
+        let mut last_debug_print_time = Instant::now();
 
         while !self.window.was_close_requested {
             self.window.clear();
             self.window.process_events();
 
-            let elapsed_time = Instant::now().checked_duration_since(last_frame_time);
+            let now = Instant::now();
+
+            let elapsed_time = now.checked_duration_since(last_frame_time);
             last_frame_time = Instant::now();
             
-            delta = delta + (elapsed_time.unwrap().as_millis() as f64) / MILLIS_PER_TICK;
+            delta = delta + (elapsed_time.unwrap().as_nanos() as f64) / NANOS_PER_TICK;
 
             while delta >= 1.0 {
                 delta -= 1.0;
                 game_state.tick();
+                tick_tracker = tick_tracker + 1;
+            }
+
+            if now.checked_duration_since(last_debug_print_time).unwrap().as_secs() >= 1
+            {
+                last_debug_print_time = now;
+
+                println!("FPS: {}", frame_tracker);
+                println!("UPS: {}", tick_tracker);
+
+                frame_tracker = 0;
+                tick_tracker = 0;
             }
 
             game_state.render();
+
+            frame_tracker = frame_tracker + 1;
 
             self.window.close();
 
